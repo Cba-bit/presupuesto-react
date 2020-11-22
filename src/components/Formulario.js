1d7988ce163f9298d2312beee06381da3dc9ef30
Ignacio <ignaciotagle26@gmail.com>
2020-11-22 17:34:31 -0300

    creando el state para los gastos, validando los gastos, creando un nuevo gasto

--- src/components/Formulario.js
+++ src/components/Formulario.js
@@ -1,15 +1,57 @@
 import React, { useState } from 'react';
+import Error from './Error';
+import shortid from 'shortid';
 
 const Formulario = () => {
+    const [nombre, setNombre] = useState('');
+    const [cantidad, setCantidad] = useState(0);
+    const [error, setError] = useState(false);
+
+    // Cuando el usuario agrega un gasto
+
+    const agregarGasto = e => {
+        e.preventDefault();
+
+        // Validar
+
+        if (cantidad < 1 || isNaN(cantidad) || nombre.trim() === '') {
+            setError(true);
+            return;
+        }
+
+        setError(false);
+
+        // construir el gasto
+
+        const gasto = {
+            nombre,
+            cantidad,
+            id: shortid.generate(),
+        };
+
+        console.log(gasto);
+
+        // pasar el gasto al componente principal
+
+        // Resetear el form
+    };
+
     return (
-        <form>
+        <form onSubmit={agregarGasto}>
             <h2>Agrega tus gastos aquí</h2>
+
+            {error ? (
+                <Error mensaje='Ambos campos son oblgatorios o Presupuesto Incorrecto' />
+            ) : null}
+
             <div className='campo'>
                 <label>Nombre Gasto</label>
                 <input
                     type='text'
                     className='u-full-width'
                     placeholder='Ej. Comida'
+                    value={nombre}
+                    onChange={e => setNombre(e.target.value)}
                 />
             </div>
 
@@ -19,6 +61,8 @@ const Formulario = () => {
                     type='number'
                     className='u-full-width'
                     placeholder='Ej. 300'
+                    value={cantidad}
+                    onChange={e => setCantidad(parseInt(e.target.value, 10))}
                 />
             </div>
 
